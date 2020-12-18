05da738ed547d0e62bf1eedb9ecf8134aa3ee386
wangjiang <wangj_0127@163.com>
2020-12-18 11:06:20 +0800

    fix: 🐛 添加斜边字边界处理

--- src/fmesh/generate/slopebackgenerator.cpp
+++ src/fmesh/generate/slopebackgenerator.cpp
@@ -15,12 +15,21 @@ namespace fmesh
 
 	void SlopebackGenerator::build()
 	{
-		double thickness = m_adParam.extend_width / 4.0;;
+		double thickness = m_adParam.extend_width / 2.0;;
 
 		std::vector<ClipperLib::PolyTree> polys(3);
 		offsetAndExtendPolyTree(m_poly, 0.0, thickness, 0, polys.at(0));
 		offsetAndExtendPolyTree(m_poly, 0.0, thickness, m_adParam.shape_bottom_height, polys.at(1));
 		offsetAndExtendPolyTree(m_poly, 0.0, thickness, m_adParam.shape_bottom_height + 1, polys.at(2));
+
+		if (m_adParam.total_height < m_adParam.shape_bottom_height)
+		{
+			offsetAndExtendPolyTree(m_poly, 0.0, thickness, m_adParam.total_height, polys.at(1));
+			_buildFromSamePolyTree(&polys.at(0), &polys.at(1));
+			_fillPolyTree(&polys.at(0));
+			_fillPolyTree(&polys.at(1));
+			return;
+		}
 
 		double slope = (m_adParam.total_height - m_adParam.shape_bottom_height - 1) / (dmax.y - dmin.y);
 		_dealPolyTreeAxisZ(&polys.at(2), -slope, m_adParam.total_height - m_adParam.shape_bottom_height - 1);
