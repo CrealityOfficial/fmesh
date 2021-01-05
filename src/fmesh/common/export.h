a4dad0f93a073223d4eeaf935dfd9755db362d6b
ZengGui <anoob@sina.cn>
2021-01-05 20:16:32 +0800

    refactor: 💡 fmesh

--- src/fmesh/common/export.h
+++ src/fmesh/common/export.h
@@ -1,17 +1,18 @@
 #ifndef FMESH_EXPORT_1603030172076_H
 #define FMESH_EXPORT_1603030172076_H
-#ifdef QT_CORE_LIB
-	#include <QtCore/QObject>
-	#ifdef FMESH_DLL
-		#define FMESH_API Q_DECL_EXPORT
-	#else
-		#define FMESH_API Q_DECL_IMPORT
-	#endif
-#else
+
+#if WIN32
 	#ifdef FMESH_DLL
 		#define FMESH_API __declspec(dllexport)
 	#else
 		#define FMESH_API __declspec(dllimport)
-	#endif			
+	#endif	
+#else
+	#ifdef FMESH_DLL
+		#define FMESH_API __attribute__((visibility("default")))
+	#else
+		#define FMESH_API
+	#endif
 #endif
+
 #endif // FMESH_EXPORT_1603030172076_H
